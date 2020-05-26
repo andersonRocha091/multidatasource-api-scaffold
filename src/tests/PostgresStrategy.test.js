5129cf6b1089154e23e6fee6db95008a7745b5ba
Anderson Rocha <andersonecomp091@gmail.com>
2020-05-26 12:13:02 -0300

    Implementing update TDD for postgres

--- src/tests/PostgresStrategy.test.js
+++ src/tests/PostgresStrategy.test.js
@@ -4,11 +4,13 @@ const Context = require("../db/strategies/base/ContextStrategy");
 
 const context = new Context(new Postgres());
 const MOCK_HEROI_CADASTRAR = { nome: "Gaviao Negro", poder: "Flechas" };
+const MOCK_HEROI_ATUALIZAR = { nome: "Mulher Maravilha", poder: "Divindade" };
 
 describe("Postgres Strategy", function () {
   this.timeout(Infinity);
   this.beforeAll(async function () {
     await context.connect();
+    await context.create(MOCK_HEROI_ATUALIZAR);
   });
 
   it("PostgresSQL Connection", async function () {
@@ -26,5 +28,19 @@ describe("Postgres Strategy", function () {
     const [result] = await context.read({ nome: MOCK_HEROI_CADASTRAR.nome });
     delete result.id;
     assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+  });
+
+  it("Atualizar Heroi", async function () {
+    const [itemAtualizar] = await context.read({
+      nome: MOCK_HEROI_ATUALIZAR.nome,
+    });
+    const novoItem = {
+      ...MOCK_HEROI_ATUALIZAR,
+      nome: "Capitã Marvel",
+    };
+    const [result] = await context.update(itemAtualizar.id, novoItem);
+    const [itemAtualizado] = await context.read({ id: itemAtualizar.id });
+    assert.deepEqual(result, 1);
+    assert.deepEqual(itemAtualizado.nome, novoItem.nome);
   });
 });
