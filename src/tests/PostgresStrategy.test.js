373b86d7249523785e9dc648cdeacb76473de8f0
Anderson Rocha <andersonecomp091@gmail.com>
2020-05-26 15:58:02 -0300

    Implementing delete method and test for postgres database

--- src/tests/PostgresStrategy.test.js
+++ src/tests/PostgresStrategy.test.js
@@ -10,6 +10,7 @@ describe("Postgres Strategy", function () {
   this.timeout(Infinity);
   this.beforeAll(async function () {
     await context.connect();
+    await context.delete();
     await context.create(MOCK_HEROI_ATUALIZAR);
   });
 
@@ -42,5 +43,11 @@ describe("Postgres Strategy", function () {
     const [itemAtualizado] = await context.read({ id: itemAtualizar.id });
     assert.deepEqual(result, 1);
     assert.deepEqual(itemAtualizado.nome, novoItem.nome);
+  });
+
+  it("Deletando por id", async function () {
+    const [item] = await context.read({});
+    const result = await context.delete(item.id);
+    assert.deepEqual(result, 1);
   });
 });
